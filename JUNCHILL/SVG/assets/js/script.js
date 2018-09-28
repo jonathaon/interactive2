58cd141acf46ffb290b21bd124713ba7b5c2f0dc
jonathaon <31681652+jonathaon@users.noreply.github.com>
2018-09-27 19:31:59 -0700

    update

---
+++ JUNCHILL/SVG/assets/js/script.js
@@ -0,0 +1,20 @@
+// $(document).ready(function(){
+
+//     console.log('hi');
+
+// 	// $(document).keypress(function(e) {
+// 	//     $('#A').append(String.fromCharCode(e.which));
+// 	//     // the keycode for 'a' is 97
+// 	//     if ( e.which == 97 ) {
+// 	//         $('#A').addClass('special');
+// 	//     }
+
+// 	// });
+
+// });
+
+$(document).ready(function(){
+	
+	console.log('hi');
+
+});
