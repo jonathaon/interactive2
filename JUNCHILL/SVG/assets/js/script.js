6afd26bafe7f903c31693b75f2a7a5401192e7c4
jonathaon <31681652+jonathaon@users.noreply.github.com>
2018-09-28 14:42:18 -0700

    update file

--- JUNCHILL/SVG/assets/js/script.js
+++ JUNCHILL/SVG/assets/js/script.js
@@ -1,20 +1,15 @@
-// $(document).ready(function(){
-
-//     console.log('hi');
-
-// 	// $(document).keypress(function(e) {
-// 	//     $('#A').append(String.fromCharCode(e.which));
-// 	//     // the keycode for 'a' is 97
-// 	//     if ( e.which == 97 ) {
-// 	//         $('#A').addClass('special');
-// 	//     }
-
-// 	// });
-
-// });
-
 $(document).ready(function(){
-	
-	console.log('hi');
+	$(document).keypress(function(e) {
 
+		// testing keypress
+		alert('keyboard clicked');
+
+	    $('#A').append(String.fromCharCode(e.which));
+
+	    // the keycode for 'a' is 97
+	    if ( e.which == 97 ) {
+	        $('#A').addClass('special');
+	    }
+
+	});
 });
