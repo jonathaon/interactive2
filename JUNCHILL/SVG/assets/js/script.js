0acbaeabcf7ca1543aab4590e8f6e1246d2aaa19
jonathaon <31681652+jonathaon@users.noreply.github.com>
2018-09-28 16:41:26 -0700

    update

--- JUNCHILL/SVG/assets/js/script.js
+++ JUNCHILL/SVG/assets/js/script.js
@@ -1,15 +1,191 @@
 $(document).ready(function(){
 	$(document).keypress(function(e) {
 
-		// testing keypress
-		alert('keyboard clicked');
 
 	    $('#A').append(String.fromCharCode(e.which));
 
 	    // the keycode for 'a' is 97
 	    if ( e.which == 97 ) {
-	        $('#A').addClass('special');
+	        $('#A').toggleClass('offsetmv');
 	    }
 
+	$('#B').append(String.fromCharCode(e.which));
+
+	    // the keycode for 'a' is 97
+	    if ( e.which == 98 ) {
+	        $('#B').toggleClass('offsetmv');
+	    }
+
+$('#C').append(String.fromCharCode(e.which));
+
+	    // the keycode for 'a' is 97
+	    if ( e.which == 99 ) {
+	        $('#C').toggleClass('offsetmv');
+	    }
+
+$('#D').append(String.fromCharCode(e.which));
+
+	    // the keycode for 'a' is 97
+	    if ( e.which == 100 ) {
+	        $('#D').toggleClass('offsetmv');
+	    }
+
+$('#E').append(String.fromCharCode(e.which));
+
+	    // the keycode for 'a' is 97
+	    if ( e.which == 101 ) {
+	        $('#E').toggleClass('offsetmv');
+	    }
+	    
+$('#F').append(String.fromCharCode(e.which));
+
+	    // the keycode for 'a' is 97
+	    if ( e.which == 102 ) {
+	        $('#F').toggleClass('offsetmv');
+	    }
+	    
+$('#G').append(String.fromCharCode(e.which));
+
+	    // the keycode for 'a' is 97
+	    if ( e.which == 103 ) {
+	        $('#G').toggleClass('offsetmv');
+	    }
+
+$('#H').append(String.fromCharCode(e.which));
+
+	    // the keycode for 'a' is 97
+	    if ( e.which == 104 ) {
+	        $('#H').toggleClass('offsetmv');
+	    }
+
+$('#I').append(String.fromCharCode(e.which));
+
+	    // the keycode for 'a' is 97
+	    if ( e.which == 105 ) {
+	        $('#I').toggleClass('offsetmv');
+	    }
+
+$('#J').append(String.fromCharCode(e.which));
+
+	    // the keycode for 'a' is 97
+	    if ( e.which == 106 ) {
+	        $('#J').toggleClass('offsetmv');
+	    }
+
+$('#K').append(String.fromCharCode(e.which));
+
+	    // the keycode for 'a' is 97
+	    if ( e.which == 107 ) {
+	        $('#K').toggleClass('offsetmv');
+	    }
+
+$('#L').append(String.fromCharCode(e.which));
+
+	    // the keycode for 'a' is 97
+	    if ( e.which == 108 ) {
+	        $('#L').toggleClass('offsetmv');
+	    } 
+
+$('#M').append(String.fromCharCode(e.which));
+
+	    // the keycode for 'a' is 97
+	    if ( e.which == 109 ) {
+	        $('#M').toggleClass('offsetmv');
+	    } 
+
+$('#N').append(String.fromCharCode(e.which));
+
+	    // the keycode for 'a' is 97
+	    if ( e.which == 110 ) {
+	        $('#N').toggleClass('offsetmv');
+	    }
+
+$('#O').append(String.fromCharCode(e.which));
+
+	    // the keycode for 'a' is 97
+	    if ( e.which == 111 ) {
+	        $('#O').toggleClass('offsetmv');
+	    }
+
+$('#P').append(String.fromCharCode(e.which));
+
+	    // the keycode for 'a' is 97
+	    if ( e.which == 112 ) {
+	        $('#P').toggleClass('offsetmv');
+	    }
+
+$('#Q').append(String.fromCharCode(e.which));
+
+	    // the keycode for 'a' is 97
+	    if ( e.which == 113 ) {
+	        $('#Q').toggleClass('offsetmv');
+	    }	
+
+$('#R').append(String.fromCharCode(e.which));
+
+	    // the keycode for 'a' is 97
+	    if ( e.which == 114 ) {
+	        $('#R').toggleClass('offsetmv');
+	    }	  
+
+$('#S').append(String.fromCharCode(e.which));
+
+	    // the keycode for 'a' is 97
+	    if ( e.which == 115 ) {
+	        $('#S').toggleClass('offsetmv');
+	    }
+
+$('#T').append(String.fromCharCode(e.which));
+
+	    // the keycode for 'a' is 97
+	    if ( e.which == 116 ) {
+	        $('#T').toggleClass('offsetmv');
+	    }
+
+$('#U').append(String.fromCharCode(e.which));
+
+	    // the keycode for 'a' is 97
+	    if ( e.which == 117 ) {
+	        $('#U').toggleClass('offsetmv');
+	    }
+
+$('#V').append(String.fromCharCode(e.which));
+
+	    // the keycode for 'a' is 97
+	    if ( e.which == 118 ) {
+	        $('#V').toggleClass('offsetmv');
+	    }
+
+
+$('#W').append(String.fromCharCode(e.which));
+
+	    // the keycode for 'a' is 97
+	    if ( e.which == 119 ) {
+	        $('#W').toggleClass('offsetmv');
+	    }	
+
+$('#X').append(String.fromCharCode(e.which));
+
+	    // the keycode for 'a' is 97
+	    if ( e.which == 120 ) {
+	        $('#X').toggleClass('offsetmv');
+	    }	 
+
+$('#Y').append(String.fromCharCode(e.which));
+
+	    // the keycode for 'a' is 97
+	    if ( e.which == 121 ) {
+	        $('#Y').toggleClass('offsetmv');
+	    }	 
+
+$('#Z').append(String.fromCharCode(e.which));
+
+	    // the keycode for 'a' is 97
+	    if ( e.which == 122 ) {
+	        $('#Z').toggleClass('offsetmv');
+	    }	             	    	    	    	          	    	    	      
 	});
+
+
+
 });
